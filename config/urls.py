52b3f6f38afe8ca5706735d70bf26fa113f5a1dc
Max <maxshapira9@gmail.com>
2023-08-01 20:08:00 +0300

    fix typo in URL pattern

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("conversation.urls")),
+    path("api/", include("conversations.urls")),
 ]
