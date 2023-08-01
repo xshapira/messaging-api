35237410490c39b2446fef847c371c94a85bfe2c
Max <maxshapira9@gmail.com>
2023-08-01 21:27:00 +0300

    define a view for user login

--- users/views.py
+++ users/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from rest_framework import status
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class LoginView(APIView):
+    """
+    Authenticate and login a user.
+
+    Handle POST request to log a user in by validating the
+    provided credentials and returning a proper response.
+
+    On successful login, a 200 response is returned indicating success.
+    Otherwise, a 401 response is returned for invalid credentials.
+    """
+
+    def post(self, request: Request) -> Response:
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return Response(
+                {"detail": "Logged in successfully."},
+                status=status.HTTP_200_OK,
+            )
+        return Response(
+            {"detail": "Invalid username or password."},
+            status=status.HTTP_401_UNAUTHORIZED,
+        )
