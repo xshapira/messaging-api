5c5dc132dd9324593b363a72bd902bb8801bbc1b
Max <maxshapira9@gmail.com>
2023-08-03 10:55:52 +0300

    add basic index.html landing page

--- config/settings.py
+++ config/settings.py
@@ -109,7 +109,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR.joinpath("templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -173,7 +173,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR.joinpath("static")
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR.joinpath("config/static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", TemplateView.as_view(template_name="index.html")),
     path("api/messages/", include("conversations.urls")),
     path("api/users/", include("users.urls")),
 ]
