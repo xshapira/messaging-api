5f3fd24dd1ad0d1110f2ba0e0f6b27ab69988077
Max <maxshapira9@gmail.com>
2023-08-03 13:43:47 +0300

    add URL route for signup view

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views import LoginView
+from users.views import LoginView, SignupView
 
 urlpatterns = [
+    path("signup/", SignupView.as_view(), name="signup"),
     path("login/", LoginView.as_view(), name="login"),
 ]
